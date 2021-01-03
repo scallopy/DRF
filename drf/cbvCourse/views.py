6838d54a0f452af76e71096df3fbf8d500c63736
scallopy <scallopy@abv.bg>
2021-01-03 12:38:13 +0200

    Using rest_framework.mixins

--- drf/cbvCourse/views.py
+++ drf/cbvCourse/views.py
@@ -6,7 +6,34 @@ from rest_framework import status
 from rest_framework.views import APIView
 from django.http import Http404
 
+from rest_framework import generics, mixins
+
 # Create your views here.
+
+class CourseList(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self, request):
+        return self.list(request)
+
+    def post(self, request):
+        return self.create(request)
+
+class CourseDetail(mixins.RetrieveModelMixin,mixins.UpdateModelMixin,mixins.DestroyModelMixin, generics.GenericAPIView):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+    def get(self, request, pk):
+        return self.retrieve(request,pk)
+
+    def put(self, request, pk):
+        return self.update(request,pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request,pk)
+
+"""
 class CourseList(APIView):
 
     def get(self, request):
@@ -46,3 +73,4 @@ class CourseDetail(APIView):
         course = self.get_object(pk)
         course.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+"""
