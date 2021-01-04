f1a45744ff14dc666b60c3cc7a999bef7c450623
scallopy <scallopy@abv.bg>
2021-01-04 14:53:26 +0200

    Admin add permission to the specific user

--- drf/order/views.py
+++ drf/order/views.py
@@ -2,14 +2,14 @@ from .models import Customer, Order
 from order.serializer import CustomerSerializer, OrderSerializer
 from rest_framework import generics
 from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 # Create your views here.
 class CustomerListView(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
     authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
