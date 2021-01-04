31cf40194b4c0dd399f4c4441e02990ec3f3b3df
scallopy <scallopy@abv.bg>
2021-01-04 15:04:09 +0200

    Global Authentications And Permissions

--- drf/drf/settings.py
+++ drf/drf/settings.py
@@ -46,6 +46,9 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS':'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE':3,
+    'DEFAULT_AUTHENTICATION_CLASSES':['rest_framework.authentication.BasicAuthentication'],
+    'DEFAULT_PERMISSIONS_CLASSES': ['rest_framework.permissions.IsAuthenticated',
+                                    'rest_framework.permissions.DjangoModelPermissions'],
 }
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
--- drf/order/views.py
+++ drf/order/views.py
@@ -1,15 +1,11 @@
 from .models import Customer, Order
 from order.serializer import CustomerSerializer, OrderSerializer
 from rest_framework import generics
-from rest_framework.authentication import BasicAuthentication
-from rest_framework.permissions import IsAuthenticated, DjangoModelPermissions
 
 # Create your views here.
 class CustomerListView(generics.ListCreateAPIView):
     queryset = Customer.objects.all()
     serializer_class = CustomerSerializer
-    authentication_classes = [BasicAuthentication]
-    permission_classes = [IsAuthenticated, DjangoModelPermissions]
 
 class CustomerDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Customer.objects.all()
